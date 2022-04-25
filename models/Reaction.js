b68f149a02722c0add4a0945e26cbc8a01148e1e
Angela Donati <angeladonati93@gmail.com>
2022-04-25 13:05:07 -0400

    add reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+// schema only 
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema({
+    reactionId: {
+        // mongoose ObjectId data type
+        type: Schema.Types.ObjectId,
+        // default value set to new ObjectId
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+},
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+module.exports = reactionSchema
