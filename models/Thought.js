cd1d2d08e5b733e2aa84f45e7c489b8c4c17df8a
Angela Donati <angeladonati93@gmail.com>
2022-04-25 13:10:32 -0400

    create Thought schema

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+// schema to create Thought model
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min_length: 1,
+            max_length: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        // nested documents
+        reactions: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'reactionSchema'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+)
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
