c6db75aadbde931b294b78d311fda6da86104967
Angela Donati <angeladonati93@gmail.com>
2022-04-30 18:42:33 -0400

    update reaction controls

--- controllers/Thoughtcontroller.js
+++ controllers/Thoughtcontroller.js
@@ -67,9 +67,35 @@ deleteThought({ params }, res) {
         res.json(thoughtData);
     })
     .catch(err => res.status(400).json(err));
+},
+// add reaction
+addReaction({params, body}, res){
+    // find Thought by id, push new reaction to reactions array
+    Thought.findOneAndUpdate({ _id: params.thoughtId}, {$push: {reactions: body}}, { new: true, runValidators: true })
+    .populate({ path: 'reactions', select: '-__v'})
+    .select('-__v')
+    .then(thoughtData => {
+        if(!thoughtData){
+            res.status(404).json({ message: 'No thought found by this ID'});
+            return; 
+        }
+        res.json(thoughtData);
+    })
+    .catch(err => res.status(400).json(err));
+},
+
+// delete reaction through id
+deleteReaction({params}, res) {
+    // find Thought by id, pull reaction from array
+    Thought.findOneAndUpdate({ _id: params.thoughtId }, { $pull: { reactions: { reactionId: params.reactionId }}}, { new : true })
+    .then(dbThoughtsData => {
+        if (!dbThoughtsData) {
+            res.status(404).json({message: 'No thoughts with this particular ID!'});
+            return;
+        }
+        res.json(dbThoughtsData);
+    })
+    .catch(err => res.status(400).json(err));
 }
-
-
-
 // end of export
 }
