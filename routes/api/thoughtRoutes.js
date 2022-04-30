efdbb7f0e0d57059cc3be5bfbe68189702f4399c
Angela Donati <angeladonati93@gmail.com>
2022-04-30 18:47:43 -0400

    import thought and reaction controller functions

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,4 +1,16 @@
 const router = require('express').Router();
 
+const  {
+createThought,
+getAllThoughts,
+getSingleThought,
+deleteThought,
+addReaction,
+deleteReaction
+} = require('../../controllers/Thoughtcontroller')
+
+// api/thoughts GET and POST route
+
+router.route('/').get(getAllThoughts).post(createThought);
 
 module.exports = router;
