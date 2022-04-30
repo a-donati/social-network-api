71ec738b8965bce5bf63dc13383ab0ff713746a5
Angela Donati <angeladonati93@gmail.com>
2022-04-30 19:47:10 -0400

    add thought and reaction seed data

---
+++ utils/thought-seeds.js
@@ -0,0 +1,53 @@
+// thought data 
+const thoughts = [
+    'I hate this website',
+    'Just existing.',
+    'I had a dream that I was a shark swimming in a basement..what does this mean?',
+    'You have been chosen. They will come soon.',
+    'The drop off has been made, you have been warned.',
+    'They are coming soon, maybe you should think twice about opening the door.',
+    'We are fixing your phone line. Do not pick up the phone the nex time it rings.',
+    'The end is near. Make preparations.',
+    'Your psychic advisors head has just exploded. Be forewarned.',
+    'My psychic advisor suggests that I keep my secrets well this month. There may be untrustworthy individuals in my environs.',
+    'Why do I have such a strong urge to wash the dishes in the bathroom sink?',
+    'Oh no, the call is dropping',
+    'Howie, I love you, but I think you are wrong',
+    'The newspaper won\'t be delivered since you already have too many.',
+    'Sorry, it turned out to be a clever hoax.'
+
+];
+
+const reactionBody = [
+    'Do you canoe?',
+    'Some say she still walks among us!',
+    'They say misery loves company, and there is certainly no shortage of misery in this household.',
+    'Say "hello" to Tragic Clown and his bag of tricks!',
+    'I swear I saw Bella Goth in Strangetown',
+    'Afraid I am being sent to military school.',
+    'Is the cake in the car?',
+    'Come with me to Gothier Green lawns',
+    'We are meeting at Gothier Green Lawns later, come through',
+    'What happened to Skip Broke?',
+    'Who removed the pool ladder while I was swimming?',
+    'Don\t kick the grave',
+    'My psychic advisor has forbidden me from leaving the house today',
+    'Be forewarned'
+]
+
+const usernames = [
+    'miss_rable',
+    'mr_hashimoto',
+    'remmucs',
+    'uLyssesSgrant',
+    'bestbuddies',
+    'mr_aaaa',
+    'emILY',
+    'Moderator',
+    'chantillie',
+    'danPort',
+    'AlexPort',
+    'Yames',
+    'hamburgler',
+    'gaston',
+]
