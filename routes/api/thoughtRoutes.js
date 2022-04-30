c5906d4cc41d6e4bfec4d12ee0178485a031eab6
Angela Donati <angeladonati93@gmail.com>
2022-04-30 19:03:46 -0400

    update thought / reaction routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -10,10 +10,18 @@ addReaction,
 deleteReaction
 } = require('../../controllers/Thoughtcontroller')
 
-// api/thoughts GET and POST route
+// /api/thoughts - GET
 router.route('/').get(getAllThoughts);
 
-// api/thoughts/:id
-router.route('/:id').get(getSingleThought)
+// /api/thoughts/:id - GET, PUT, DELETE
+router.route('/:id').get(getSingleThought).put(updateThought).delete(deleteThought)
 
+// /api/thoughts/:userId - POST
+router.route('/:userId').post(createThought);
+
+// /api/thoughts/:thoughtId/reactions - POST
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/reactionId - DELETE
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction)
 module.exports = router;
